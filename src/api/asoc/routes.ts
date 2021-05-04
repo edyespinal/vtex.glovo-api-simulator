8c7cf3c759f6b60ff1bbd9040302cfff35ded0b6
edyespinal <contacto@edyespinal.com>
2021-05-04 15:47:10 +0200

    fix: add auth routes to existing routes

--- src/api/asoc/routes.ts
+++ src/api/asoc/routes.ts
@@ -1,9 +1,12 @@
-import { Router } from 'express';
-import { feedDb, findAll } from './controller';
+import { Router } from 'express'
+import { feedDb, findAll } from './controller'
+import { signin, signup } from '../../auth/controller'
 
-const router = Router();
+const router = Router()
 
-router.get('/feed', feedDb);
-router.get('/', findAll);
+router.get('/feed', feedDb)
+router.get('/', findAll)
+router.post('/signin', signin)
+router.post('/signup', signup)
 
-export default router;
+export default router
