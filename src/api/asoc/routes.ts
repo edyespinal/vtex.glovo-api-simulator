06d7bf7e504fbb0f656409bd664ddee331566f97
edyespinal <contacto@edyespinal.com>
2021-05-04 16:04:20 +0200

    fix: remove test URLs

--- src/api/asoc/routes.ts
+++ src/api/asoc/routes.ts
@@ -1,12 +1,9 @@
 import { Router } from 'express'
 import { feedDb, findAll } from './controller'
-import { signin, signup } from '../../auth/controller'
 
 const router = Router()
 
 router.get('/feed', feedDb)
 router.get('/', findAll)
-router.post('/signin', signin)
-router.post('/signup', signup)
 
 export default router
